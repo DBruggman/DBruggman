b3958e4a6fc9299ad8ec247db18e6339f78fbacc
Drew Bruggman <drewbruggman@gmail.com>
2024-11-27 15:24:04 -0800

    Vite + React structure introduced

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1>oh hai</h1>
+    <App />
+  </StrictMode>,
+)
